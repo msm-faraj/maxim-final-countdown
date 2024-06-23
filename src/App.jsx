e514716c3f5ea1755f935e42c3cbd0973caefb23
msm-faraj <farajzademeysam@gmail.com>
2024-06-23 12:02:42 +0200

    adding time challenge component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimeChallange from "./components/TimeChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimeChallange title="Easy" targetTime={1} />
+        <TimeChallange title="Not Easy" targetTime={5} />
+        <TimeChallange title="Getting Tough" targetTime={10} />
+        <TimeChallange title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
